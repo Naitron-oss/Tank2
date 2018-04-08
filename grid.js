90125212de197df587a9b000ccb489a043eee5f8
aadijain <jain.aadi@gmail.com>
2018-04-09 02:53:13 +0530

    wall positioning

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 function randmap(){
-    if(random() > 0.5)
+    if(random() < 0.5)
         return map1();
     else
         return map2();
@@ -7,40 +7,40 @@ function randmap(){
 
 function map1(){
     var map = [];
-    for(var i = -10; i < width; i += 300){
-        for(var j = -10; j < height; j += 300){
-            if(random() > 0.67){
-                map.push(new Wall(i, j, 20, 80));
-                map.push(new Wall(i, j, 80, 20));
+    for(var i = 0; i < width + 20; i += 300){
+        for(var j = 0; j < height + 20; j += 300){
+            if(random() < 0.4){
+                map.push(new Wall(i, j, 20, 90));
+                map.push(new Wall(i, j, 90, 20));
             }
-            else if(random() > 0.67){
+            else if(random() < 0.25){
                 map.push(new Wall(i, j, 10, 10, -1.5, 'blue'));
             }
         }
     }
-    map.push(new Wall(width/2, -10, width/2, 20));
-    map.push(new Wall(width/2, height+10, width/2, 20));
-    map.push(new Wall(-10, height / 2, 20, height / 2));
-    map.push(new Wall(width+10, height/2, 20, height/2));
+    map.push(new Wall(width/2, 0, width/2, 20));
+    map.push(new Wall(width/2, height, width/2, 20));
+    map.push(new Wall(0, height/2, 20, height/2));
+    map.push(new Wall(width, height/2, 20, height/2));
     return map;
 }
 
 function map2() {
     var map = [];
     
-    for (var i = -10; i < width; i += 250) {
-        for (var j = -10; j < height; j += 250) {
-            if(random() > 0.8)
+    for (var i = 0; i < width + 20; i += 250) {
+        for (var j = 0; j < height + 20; j += 250) {
+            if(random() < 0.2)
                 map.push(new Wall(i, j, 10, 10, -1.5, 'blue'));
-            else if (random() > 0.5)
+            else if (random() < 0.5)
                 map.push(new Wall(i, j, 20, 90));
-            else if (random() > 0)
+            else
                 map.push(new Wall(i, j, 90, 20));
         }
     }
-    map.push(new Wall(width / 2, -10, width / 2, 20));
-    map.push(new Wall(width / 2, height + 10, width / 2, 20));
-    map.push(new Wall(-10, height / 2, 20, height / 2));
-    map.push(new Wall(width + 10, height / 2, 20, height / 2));
+    map.push(new Wall(width / 2, 0, width / 2, 20));
+    map.push(new Wall(width / 2, height, width / 2, 20));
+    map.push(new Wall(0, height / 2, 20, height / 2));
+    map.push(new Wall(width, height / 2, 20, height / 2));
     return map;
 }
